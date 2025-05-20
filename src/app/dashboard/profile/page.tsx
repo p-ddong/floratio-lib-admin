f882758c9573d1725e8cad2cc5d406379aaf37f6
p-ddong <hankrezu@gmail.com>
2025-05-20 21:57:11 +0700

    [Fix]: Hieu nul

--- src/app/dashboard/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const profile = () => {
-  return (
-    <div style={{ marginLeft: '240px', padding: '1rem' }}>profile</div>
-  )
-}
-
-export default profile
--- src/app/dashboard/user/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const user = () => {
-  return (
-    <div style={{ marginLeft: '240px', padding: '1rem' }}>user</div>
-  )
-}
-
-export default user
